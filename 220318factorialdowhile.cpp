6f5f737127c3e0724c9ce0fcb7f9283499614704
majocastro03 <101900997+majocastro03@users.noreply.github.com>
2022-03-25 14:46:52 -0500

    Update 220318factorialdowhile.cpp

--- 220318factorialdowhile.cpp
+++ 220318factorialdowhile.cpp
@@ -1,23 +1,24 @@
+
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char** argv) {
 	int N;
- 	int long long F= 1;
+ 	int long long F;
 	int otro=1;
-	
-	
-	cout<<"Ingrese un numero; \n";
+    do{
+    F = 1;
+	cout<<"Ingrese un numero: \n";
 	cin>>N;
-	
-do{ 
+    do{ 
 	 F= (F*N);
 	 N= (N-1);
-}
-while(N > 1);
-
-	cout<<"Su factorial es: "<<F<<"\n";
-	
+    }while(N > 1);
+    cout<<"Su factorial es: "<<F<<"\n";
+    cout<<"Si desea otro ingrese 1: ";
+    cin>>otro;
+    }while(otro==1);
 	return 0;
 }
+
